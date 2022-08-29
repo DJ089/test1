e0bd251ddd6ef203f04c36bff502d228ec268d07
DJ089 <dongju089@gmail.com>
2022-08-29 14:46:16 +0900

    t20220829

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <KossieCoder>
+      <template #header="{kossie}">
+        <p>header11 {{kossie}}</p>
+      </template>
+      <template  #default>
+        hello
+
+      </template>
+      
+    </KossieCoder>
   </div>
 </template>
+
+<script>
+import KossieCoder from '@/components/KossieCoder.vue';
+
+export default {
+  components: {
+    KossieCoder,
+    
+}
+}
+</script>
